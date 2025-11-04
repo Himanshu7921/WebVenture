b61a547a356ae0accf4d8e81860bd5b4d20fa5c2
Kalashkhatod <kalashkhatod@gmail.com>
2025-11-04 19:41:16 +0530

    Modified grid.jsx

--- TAILWIND/tailfour/src/grid.jsx
+++ TAILWIND/tailfour/src/grid.jsx
@@ -37,7 +37,7 @@ return (
   </div>
   <h3 className="text-gray-900  mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
   <p className="text-gray-500  mt-2 text-sm ">
-    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+    The Zero  Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
   </p>
 </div>
 </div>
