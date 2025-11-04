d799db8ecf243031cb01649bf6ce3a0f2eadd108
Kalashkhatod <kalashkhatod@gmail.com>
2025-11-05 04:02:29 +0530

    Modified grid.jsx

--- TAILWIND/tailfour/src/grid.jsx
+++ TAILWIND/tailfour/src/grid.jsx
@@ -12,35 +12,14 @@ export default function Login(props) {
 
 
 return (
-<div className=" bg-gray-900 text-white">
-    <p className=" sm:text-3xl text-sm ">Hello this is goat</p>
-    <div className="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-  <div>
-    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
-      <svg className="h-6 w-6 stroke-white" >
 
-      </svg>
-    </span>
-  </div>
-  <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-  <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-   The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-  </p>
+<div className="m-4 grid grid-cols-1 gap-4 sm:grid-cols-12 ">
+  <div className="min-h-[100px] col-span-12 rounded-lg bg-orange-500 shadow sm:col-span-2"></div>
+  <div className="min-h-[100px] col-span-12 rounded-lg bg-teal-500 shadow sm:col-span-10"></div>
+  <div className="min-h-[100px] col-span-12 rounded-lg bg-red-500 shadow sm:col-span-6"></div>
+  <div className="min-h-[100px] rounded-lg bg-purple-500 shadow sm:col-span-6 sm:block hidden"></div>
 </div>
-<div class="bg-white rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-  <div>
-    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
-      <svg className="h-6 w-6 stroke-white" >
 
-      </svg>
-    </span>
-  </div>
-  <h3 className="text-gray-900  mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-  <p className="text-gray-500  mt-2 text-sm ">
-    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-  </p>
-</div>
-</div>
 
 
   );
