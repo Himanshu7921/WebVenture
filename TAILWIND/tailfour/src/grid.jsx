26b872a6f0c48aa388060d9824a1e24496283373
Kalashkhatod <kalashkhatod@gmail.com>
2025-11-08 19:21:13 +0530

    grid.jsx

--- TAILWIND/tailfour/src/grid.jsx
+++ TAILWIND/tailfour/src/grid.jsx
@@ -18,7 +18,7 @@ return (
   <div className="min-h-[100px] col-span-12 rounded-lg bg-teal-500 shadow sm:col-span-10"></div>
   <div className="min-h-[100px] col-span-12 rounded-lg bg-red-500 shadow sm:col-span-6"></div>
   <div className="min-h-[100px] rounded-lg bg-purple-500 shadow sm:col-span-6 sm:block hidden"></div>
-</div>
+ </div>
 
 
 
