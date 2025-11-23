5f919e821e4bf5c6aab34f43ebef2880b8c3e8a4
Kalashkhatod <kalashkhatod@gmail.com>
2025-11-24 03:39:28 +0530

    Trying new things in hello.js

--- JSFOUNDATION/hello.js
+++ JSFOUNDATION/hello.js
@@ -239,3 +239,48 @@ checkData();
 //   default:
 //     statementsDefault;
 // }
+
+let fruitType = "Bananas";
+switch (fruitType) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Apples":
+    console.log("Apples are $0.32 a pound.");
+    break;
+  case "Bananas":
+    console.log("Bananas are $0.48 a pound.");
+    break;
+  case "Cherries":
+    console.log("Cherries are $3.00 a pound.");
+    break;
+  case "Mangoes":
+    console.log("Mangoes are $0.56 a pound.");
+    break;
+  case "Papayas":
+    console.log("Papayas are $2.79 a pound.");
+    break;
+  default:
+    console.log(`Sorry, we are out of ${fruitType}.`);
+}
+console.log("Is there anything else you'd like?");
+
+
+
+// To throw error 
+// throw {
+//   toString() {
+//     return console.log("I'm an object!");
+//   },
+// };
+
+// console.log("Hello");
+
+// throw 42;
+// var f = 49;
+// console.log(f);
+
+
+
+
+
