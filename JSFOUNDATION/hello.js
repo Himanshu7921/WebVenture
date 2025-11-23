e684e2b0a502fe8ad70b7c52e353d23c7884a1f5
Kalashkhatod <kalashkhatod@gmail.com>
2025-11-23 14:32:13 +0530

    Made some changed in hello.js

--- JSFOUNDATION/hello.js
+++ JSFOUNDATION/hello.js
@@ -70,6 +70,172 @@ console.log(car.special); // Toyota
 console.log(carTypes("Kids specially to himanshu singh"));
 console.log("37" - 7); // 30
 console.log("37" + 7); // 377
- "37" * 7; // 259
+"37" * 7; // 259
+
+const carr = { manyCars: { a: "Saab", b: "Jeep" }, 7: "Mazda" };
+
+console.log(carr.manyCars.b); // Jeep
+console.log(carr[7]); // Mazda
 
 
+
+
+
+const theProtoObj = {
+  toString() {
+    return "I am the parent";
+  }
+};
+
+const handler = "hello";
+
+const obj = {
+  __proto__: theProtoObj,
+  handler,
+  toString() {
+    return `d ${super.toString()}`;
+  },
+  ["prop_" + (() => 42)()]: "hello",
+};
+
+console.log(obj.toString()); // d I am the parent
+console.log(obj.prop_42);    // hello
+
+
+
+
+var name2 = "Kalash";
+console.log(`Hello ${name2}`);
+
+
+
+
+const formatArg = (arg) => {
+  if (Array.isArray(arg)) {
+    // Print a bulleted list
+    return arg.map((part) => `- ${part}`).join("\n");
+  }
+  if (arg.toString === Object.prototype.toString) {
+    // This object will be serialized to "[object Object]".
+    // Let's print something nicer.
+    return JSON.stringify(arg);
+  }
+  return arg;
+};
+
+
+
+
+
+
+const print = (segments, ...args) => {
+  // For any well-formed template literal, there will always be N args and
+  // (N+1) string segments.
+  let message = segments[0];
+  segments.slice(1).forEach((segment, index) => {
+    message += formatArg(args[index]) + segment;
+  });
+  console.log(message);
+};
+
+const todos = [
+  "Learn JavaScript",
+  "Learn Web APIs",
+  "Set up my website",
+  "Profit!",
+];
+
+const progress = { javascript: 20, html: 50, css: 10 };
+
+print`I need to do:
+${todos}
+My current progress is: ${progress}
+`;
+
+// I need to do:
+// - Learn JavaScript
+// - Learn Web APIs
+// - Set up my website
+// - Profit!
+// My current progress is: {"javascript":20,"html":50,"css":10}
+
+
+
+
+
+const str =
+  "this string \
+is broken \
+across multiple \
+lines.";
+console.log(str); // this string is broken across multiple lines.
+
+
+
+
+var z = 1;
+{
+var z = 2;
+}
+console.log(z); // 2
+
+
+
+// while (x < 10) {
+//   x++;
+// }
+
+// if (condition1) {
+//   statement1;
+// } else if (condition2) {
+//   statement2;
+// } else if (conditionN) {
+//   statementN;
+// } else {
+//   statementLast;
+// }
+
+
+
+const b = new Boolean(false);
+if (b) {
+  // this condition evaluates to true
+}
+if (b == true) {
+  // this condition evaluates to false
+}
+
+
+
+  // ⭐ Your validation function
+  function checkData() {
+    if (document.form1.threeChar.value.length === 3) {
+      print`Valid input: ${document.form1.threeChar.value}`;
+      return true;
+    }
+
+    alert(
+      `Enter exactly three characters. ${document.form1.threeChar.value} is not valid.`
+    );
+
+    print`Invalid input: ${document.form1.threeChar.value}`;
+    return false;
+  }
+
+document.form1 = { threeChar: { value: "Her" } };
+checkData();
+
+
+
+
+// switch (expression) {
+//   case label1:
+//     statements1;
+//     break;
+//   case label2:
+//     statements2;
+//     break;
+//   // …
+//   default:
+//     statementsDefault;
+// }
