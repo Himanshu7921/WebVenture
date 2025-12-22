93c26c4bbca49558cc571362e52231bac7458ca3
Kalashkhatod <kalashkhatod@gmail.com>
2025-12-23 00:40:41 +0530

    Learning DOM Manipulation

--- JSFOUNDATION/DOM/app.js
+++ JSFOUNDATION/DOM/app.js
@@ -24,7 +24,7 @@ const title = document.querySelector('#main-heading');
 title.style.color = 'red';
 console.log(title);
 
-const listitems = document.querySelectorAll('.list-items');
+const listitems = document.querySelectorAll('.list-items') //.forEach(item => item.style.fontSize = '1rem'); //quick way
 // listitems.style.fontSize =  '1rem';  //it is called camelCase in js
 //Above code wont change fontSize of list items 
 //We need loop
@@ -35,3 +35,38 @@ for(let i = 0; i < listitems.length; i++){
 
 console.log(listitems);
 
+
+// Creating Elements
+
+const ul = document.querySelector('ul');
+
+const newLi = document.createElement("li");
+newLi.textContent = "Inception";
+newLi.classList.add("list-items");
+
+ul.append(newLi);
+
+// Modifying the text
+
+const firstListItem = document.querySelector('.list-items');
+
+console.log(firstListItem.innerText);
+console.log(firstListItem.textContent);
+console.log(firstListItem.innerHTML);
+
+newLi.innerText = "X-men"; // Inception -> X-men
+
+newLi.setAttribute('id', 'main-heading'); //font-style = italic
+newLi.removeAttribute('id'); // Returned to normal font 
+
+const titlee = document.querySelector('#main-heading');
+
+console.log(titlee.getAttribute('id'));
+
+// Remove class which we added previously on newLi
+newLi.classList.remove('list-items');
+
+console.log(newLi.classList.contains('list-items')); // Returns True or false
+
+// Remove Elements
+newLi.remove(); // comment this to see newLi
---
+++ JSFOUNDATION/DOM/app1.js
@@ -0,0 +1,13 @@
+// DOM manipulation
+
+// Traverse the DOM
+
+// Parent Node Traversal
+let ul = document.querySelector('ul');
+console.log(ul.parentNode.parentNode.parentNode.parentNode);
+console.log(ul.parentElement.parentElement.parentElement.parentElement);
+
+
+// Child Node Traversal
+
+// Sibling Node Traversal
