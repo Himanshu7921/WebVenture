6a58e0cbddd69e9f9f9e2ceafbfdd46f1af56974
Kalashkhatod <kalashkhatod@gmail.com>
2025-12-22 14:49:33 +0530

    Learning DOM manipulation

---
+++ JSFOUNDATION/DOM/app.js
@@ -0,0 +1,37 @@
+// DOM manipulation
+
+// // GetElementById()
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// // GetElementByClassName()
+// // const listitems = document.getElementsByClassName('list-items');
+// // console.log(listitems);
+
+// // getElementsByTagName()
+// const listitem = document.getElementsByTagName('li');
+// console.log(listitem);
+
+// // querySelector()
+// const container = document.querySelector('div');
+// console.log(container); //prints only first container
+
+// // querySelectorAll()
+// const container_all = document.querySelectorAll('div');
+// console.log(container_all); //prints all
+
+const title = document.querySelector('#main-heading');
+title.style.color = 'red';
+console.log(title);
+
+const listitems = document.querySelectorAll('.list-items');
+// listitems.style.fontSize =  '1rem';  //it is called camelCase in js
+//Above code wont change fontSize of list items 
+//We need loop
+
+for(let i = 0; i < listitems.length; i++){
+    listitems[i].style.fontSize = '1rem';
+}
+
+console.log(listitems);
+
