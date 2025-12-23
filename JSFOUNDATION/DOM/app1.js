600e90e23b42ea5d99c6a3b1794f510e89496f74
Kalashkhatod <kalashkhatod@gmail.com>
2025-12-23 10:53:28 +0530

    Learning DOM manipulation

--- JSFOUNDATION/DOM/app1.js
+++ JSFOUNDATION/DOM/app1.js
@@ -3,11 +3,27 @@
 // Traverse the DOM
 
 // Parent Node Traversal
-let ul = document.querySelector('ul');
-console.log(ul.parentNode.parentNode.parentNode.parentNode);
-console.log(ul.parentElement.parentElement.parentElement.parentElement);
+// let ul = document.querySelector('ul');
+// console.log(ul.parentNode.parentNode.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement.parentElement.parentElement);
 
-
+// const html = document.documentElement;
+// console.log(html.parentNode); //#document
+// console.log(html.parentElement); //Null
 // Child Node Traversal
 
+let ul = document.querySelector('ul');
+// console.log(ul.childNodes);
+// console.log(ul.lastChild);
+
+// ul.firstChild.textContent = 'hello';
+// ul.childNodes[1].style.backgroundColor = 'blue'; // cannot apply on text node
+
+console.log(ul.children);
+console.log(ul.firstElementChild);
+console.log(ul.lastElementChild);
+
+
+
 // Sibling Node Traversal
+
