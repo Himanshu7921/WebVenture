f3cc6409cdc3fea195896d5fe3969106aaa4fa04
Kalashkhatod <kalashkhatod@gmail.com>
2025-12-24 22:59:39 +0530

    Learning DOM Manipulation

--- JSFOUNDATION/DOM/app2.js
+++ JSFOUNDATION/DOM/app2.js
@@ -20,5 +20,18 @@ function changeBgColor(){
     newBackgroundColor.style.backgroundColor ='blue';
 }
 
-newBackgroundColor.addEventListener("mouseover",changeBgColor)
+newBackgroundColor.addEventListener("mouseover",changeBgColor);
 
+const revealBtn = document.querySelector('.reveal-btn');
+
+const hiddenContent = document.querySelector('.hidden-content');
+
+function revealContent(){
+    if(hiddenContent.classList.contains('reveal-btn')){
+        hiddenContent.classList.remove('reveal-btn')
+    }else{
+        hiddenContent.classList.add('reveal-btn')
+    }
+}
+
+revealBtn.addEventListener("click",revealContent);
