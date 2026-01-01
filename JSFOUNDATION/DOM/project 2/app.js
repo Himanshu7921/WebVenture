bf15e3825c0293c61e3dad2cefd2e5855101af9c
Kalashkhatod <kalashkhatod@gmail.com>
2026-01-01 22:42:23 +0530

    Modified project 2

--- JSFOUNDATION/DOM/project 2/app.js
+++ JSFOUNDATION/DOM/project 2/app.js
@@ -14,7 +14,7 @@ closeBtn.addEventListener('click', function(){
     modalContainer.style.display = 'none';
 })
 
-window.addEventListener('click', function(e){
+ window.addEventListener('click', function(e){
 
     if (e.target === modalContainer){
         modalContainer.style.display = 'none';
